7611424d2e53321771679ff9f552ef4b61f81874
Hongbin Li <hblee12294@gmail.com>
2020-04-01 23:07:05 -0700

    feat: update quick_sort

--- problems/bd/quick_sort.js
+++ problems/bd/quick_sort.js
@@ -18,12 +18,45 @@ function quickSort(arr) {
   return quickSort(left).concat(pivot, quickSort(right))
 }
 
+function quickSort2(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let mid = sortAndFindMid(arr, left, right)
+
+    quickSort2(arr, left, mid - 1)
+    quickSort2(arr, mid + 1, right)
+  }
+
+  return arr
+}
+
+function sortAndFindMid(arr, left, right) {
+  const base = arr[left]
+  let index = left + 1
+
+  for (let i = index; i <= right; i++) {
+    if (base > arr[i]) {
+      swap(arr, index, i)
+      index++
+    }
+  }
+
+  swap(arr, index - 1, left)
+
+  return index - 1
+}
+
+function swap(arr, left, right) {
+  let temp = arr[right]
+  arr[right] = arr[left]
+  arr[left] = temp
+}
+
 const input = [6, 1, 2, 7, 9, 4, 5, 10, 8]
-const result = quickSort(input)
+const result = quickSort2(input)
 
 console.log(JSON.stringify(result))
 
 const input2 = [98, 42, 25, 54, 15, 3, 25, 72, 41, 10, 121]
-const result2 = quickSort(input2)
+const result2 = quickSort2(input2)
 
 console.log(JSON.stringify(result2))
