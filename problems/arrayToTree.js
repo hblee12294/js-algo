a8930cfe17f474cfc961c8c57bc470b5c5e50479
Hongbin Li <hblee12294@gmail.com>
2022-08-04 01:37:36 -0700

    feat: add like.html

---
+++ problems/arrayToTree.js
@@ -0,0 +1,17 @@
+let arr = [
+  { id: 1, name: "部门1", pid: 0 },
+  { id: 2, name: "部门2", pid: 1 },
+  { id: 3, name: "部门3", pid: 1 },
+  { id: 4, name: "部门4", pid: 3 },
+  { id: 5, name: "部门5", pid: 4 },
+];
+
+const arrayToTree = (arr) => {
+  const result = []
+
+  
+
+  return result
+};
+
+const getChildren = item;
