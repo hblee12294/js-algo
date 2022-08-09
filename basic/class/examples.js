fc22dcbe634d28f449eb1fe593f62612c4dbf300
Hongbin Li <hblee12294@gmail.com>
2022-08-09 00:23:52 -0700

    feat: add class example

---
+++ basic/class/examples.js
@@ -0,0 +1,17 @@
+class Button {
+  constructor(value) {
+    this.value = value;
+  }
+
+  // click() {
+  //   console.log(this.value);
+  // }
+
+  click = () => {
+    console.log(this.value);
+  }
+}
+
+let button = new Button("hello");
+
+setTimeout(button.click, 1000); // undefined
