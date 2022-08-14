c7249bbb83f84e188ceebad9606150480f412807
Hongbin Li <hblee12294@gmail.com>
2022-08-13 23:43:22 -0700

    feat: add lc20

---
+++ lc/20 Valid Parentheses.ts
@@ -0,0 +1,23 @@
+function isValid(s: string): boolean {
+  const leftChars = ["{", "(", "["];
+  const rightChars = ["}", ")", "]"];
+
+  if (s.length === 0) return false;
+
+  if (s.length % 2 === 1) return false;
+
+  const stack: string[] = [];
+
+  for (let i = 0; i < s.length; ++i) {
+    if (leftChars.includes(s[i])) {
+      stack.push(s[i]);
+    } else {
+      const top = stack.pop();
+      const leftIndex = leftChars.findIndex((char) => char === top);
+      const rightIndex = rightChars.findIndex((char) => char === s[i]);
+      if (leftIndex !== rightIndex) return false;
+    }
+  }
+
+  return !stack.length;
+}
